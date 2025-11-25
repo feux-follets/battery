daa36eda3bc98ddf8b125f76a4177d4e09955c1a
Evan Lee <feuxfollets1228@gmail.com>
2025-11-24 21:41:29 -0500

    Use CHTE for Sequoia >= 15.1.1OC

--- battery.sh
+++ battery.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
 
 smc_cmd="/usr/local/bin/smc"
+smc_key="CHTE"
+
 current_date=$(date '+%Y-%m-%d %H:%M:%S')
 maintain_percent=80
+sail_percent=50
 
-battery_percent=$(pmset -g batt | tail -n1 | awk '{print $3}' | tr -d '%;')
+function get_accurate_battery_percentage() {
+	MaxCapacity=$(ioreg -l -n AppleSmartBattery -r | grep "\"AppleRawMaxCapacity\" =" | awk '{ print $3 }' | tr ',' '.')
+	CurrentCapacity=$(ioreg -l -n AppleSmartBattery -r | grep "\"AppleRawCurrentCapacity\" =" | awk '{ print $3 }' | tr ',' '.')
+	accurate_battery_percentage=$(echo "$CurrentCapacity*100/$MaxCapacity" | bc)
+	echo "$accurate_battery_percentage"
+}
+
+battery_percent=$(get_accurate_battery_percentage)
 echo "$current_date: Battery percent: $battery_percent%"
-ch0c=$($smc_cmd -k CH0C -r | awk '{print $4}' | tr -d ')')
+
+chte=$($smc_cmd -k "$smc_key" -r | awk '{print $(NF-3)}')
 
 if [ "$battery_percent" -ge $maintain_percent ]; then
-	if [ "$ch0c" -eq 00 ]; then
-		echo "$current_date: Battery is >= $maintain_percent%. Enabling CH0C."
-		$smc_cmd -k CH0C -w 01
+	if [ "$chte" -eq 00 ]; then
+		echo "$current_date: Battery is >= $maintain_percent%. Disable charging."
+		$smc_cmd -k "$smc_key" -w 01000000
 	else
-		echo "$current_date: CH0C already enabled."
+		echo "$current_date: Charging already disabled."
 	fi
-elif [ "$battery_percent" -lt $maintain_percent ]; then
-	if [ "$ch0c" -eq 01 ]; then
-		echo "$current_date: Battery is < $maintain_percent%. Disabling CH0C."
-		$smc_cmd -k CH0C -w 00
+else
+	if [ "$battery_percent" -ge $sail_percent ]; then
+		if [ "$chte" -eq 01 ]; then
+			echo "$current_date: Battery is >= $sail_percent%. Battery is not charging."
+		else
+			echo "$current_date: Battery is >= $sail_percent%. Battery is charging."
+		fi
 	else
-		echo "$current_date: CH0C already disabled."
+		if [ "$chte" -eq 01 ]; then
+			echo "$current_date: Battery is < $sail_percent%. Enable charging."
+			$smc_cmd -k "$smc_key" -w 00000000
+		else
+			echo "$current_date: Charging already enabled."
+		fi
 	fi
 fi
 
